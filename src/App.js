a90f9219ecec67d858d92601024a5294b65ef9f5
Sina <sinasteyn@gmail.com>
2023-07-13 21:21:12 +0330

    initial CRA initial tailwindcss

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <h1 className="text-center text-lg text-lime-950 bg-lime-50 h-[50px]">
+      Course online shop
+    </h1>
+  );
+}
+
+export default App;
